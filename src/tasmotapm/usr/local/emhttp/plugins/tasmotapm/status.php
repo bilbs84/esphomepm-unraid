fe68aba0c8ff9b04ee5b1373d5394c290077bd85
bilbs84 <99056315+bilbs84@users.noreply.github.com>
2024-07-18 05:18:08 +1000

    Update status.php
    Modify for ESPHome REST API

--- src/tasmotapm/usr/local/emhttp/plugins/tasmotapm/status.php
+++ src/tasmotapm/usr/local/emhttp/plugins/tasmotapm/status.php
@@ -6,44 +6,38 @@ $tasmotapm_device_user	= isset($tasmotapm_cfg['DEVICE_USER']) ? $tasmotapm_cfg['
 $tasmotapm_device_pass	= isset($tasmotapm_cfg['DEVICE_PASS']) ? $tasmotapm_cfg['DEVICE_PASS'] : "";
 $tasmotapm_costs_price	= isset($tasmotapm_cfg['COSTS_PRICE']) ? $tasmotapm_cfg['COSTS_PRICE'] : "0.0";
 $tasmotapm_costs_unit	= isset($tasmotapm_cfg['COSTS_UNIT']) ? $tasmotapm_cfg['COSTS_UNIT'] : "USD";
+$esphome_device_ip = isset($tasmotapm_cfg['DEVICE_IP']) ? $tasmotapm_cfg['DEVICE_IP'] : "";
 
-
-if ($tasmotapm_device_ip == "") {
-	die("Tasmota Device IP missing!");
+if ($esphome_device_ip == "") {
+    die("ESPHome Device IP missing!");
 }
 
-$Url = "http://" . $tasmotapm_device_ip . "/cm?";
+// Define sensor endpoints
+$sensors = [
+    'Power' => "/sensor/power",
+    'Voltage' => "/sensor/voltage",
+    'Current' => "/sensor/current",
+    'Today' => "/sensor/today",
+    'Factor' => "/sensor/factor"
+];
 
-if ($tasmotapm_use_pass == 1) {
-	if ($tasmotapm_device_user == "") {
-		die("Tasmota username missing!");
-	}
-	if ($tasmotapm_device_pass == "") {
-		die("Tasmota password missing!");
-	}
-
-	$Url = $Url . "user=" . $tasmotapm_device_user . "&password=" . $tasmotapm_device_pass . "&";
+$json = [];
+foreach ($sensors as $key => $endpoint) {
+    $Url = "http://$esphome_device_ip$endpoint";
+    $datajson = file_get_contents($Url);
+    $data = json_decode($datajson, true);
+    
+    if (isset($data['state'])) {
+        $json[$key] = $data['state'];
+    } else {
+        $json[$key] = null; // Handle missing data
+    }
 }
 
-$Url = $Url . "cmnd=Status%208";
-
-$datajson = file_get_contents($Url);
-$data = json_decode($datajson, true); 
-
-$json = array(
-		'Total' => $data['StatusSNS']['ENERGY']['Total'],
-		'Today' => $data['StatusSNS']['ENERGY']['Today'],
-		'Yesterday' => $data['StatusSNS']['ENERGY']['Yesterday'],
-		'Voltage' => $data['StatusSNS']['ENERGY']['Voltage'],
-		'Current' => $data['StatusSNS']['ENERGY']['Current'],
-		'ApparentPower' => $data['StatusSNS']['ENERGY']['ApparentPower'],
-		'ReactivePower' => $data['StatusSNS']['ENERGY']['ReactivePower'],
-		'Factor' => $data['StatusSNS']['ENERGY']['Factor'],
-		'Power' => $data['StatusSNS']['ENERGY']['Power'],
-		'Costs_Price' => $tasmotapm_costs_price,
-		'Costs_Unit' => $tasmotapm_costs_unit
-	);
+$json['Costs_Price'] = $tasmotapm_costs_price;
+$json['Costs_Unit'] = $tasmotapm_costs_unit;
 
 header('Content-Type: application/json');
 echo json_encode($json);
+
 ?>
